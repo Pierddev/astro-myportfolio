a35ead6db3aff1e9a9806bfef20aea91813979db
Pierddev <139257364+Pierddev@users.noreply.github.com>
2025-05-13 19:46:14 +0200

    Commit 25-05-013a

---
+++ src/data/experiences.ts
@@ -0,0 +1,199 @@
+import logoPicode from "@assets/logo-picodestudio.webp";
+import logoCA from "@assets/logo-creditagricole.webp";
+import logoKingup from "@assets/logo-kingup.webp";
+import logoLAG from "@assets/logo-lag.webp";
+import logoPythagore from "@assets/logo-pythagore.webp";
+
+export const experiences = [
+  {
+    date: "AVRIL 2025 - AUJOURD'HUI",
+    poste: "Développeur Web Freelance",
+    entreprise: "Pi-Code Studio",
+    lieu: "Acigné (35)",
+    logoSrc: logoPicode,
+    logoAlt: "Logo de Pi-Code Studio",
+    missions: [
+      {
+        title: "Analyse des besoins clients :",
+        description:
+          "Recueil des attentes, étude de faisabilité et rédaction de cahiers des charges adaptés à chaque projet.",
+      },
+      {
+        title: "Conception et développement d’applications web :",
+        description:
+          "Réalisation de solutions sur mesure (applications métier, sites vitrines, outils internes), de la structure technique au design responsive.",
+      },
+      {
+        title: "Développement Full Stack :",
+        description:
+          "Maîtrise des technologies front-end et back-end pour assurerla cohérence et la performance des projets du début à la mise en production.",
+      },
+      {
+        title: "Suivi de projet et relation client :",
+        description:
+          "Communication régulière avec les clients, accompagnement, ajustements et mise en ligne des livrables.",
+      },
+    ],
+  },
+  {
+    date: "SEPTEMBRE 2024 - MARS 2025",
+    poste: "Conseiller des particuliers",
+    entreprise: "Crédit Agricole",
+    lieu: "Noyal-sur-Vilaine (35)",
+    logoSrc: logoCA,
+    logoAlt: "Logo du Crédit Agricole",
+    missions: [
+      {
+        title: "Gestion et développement d’un portefeuille clients :",
+        description:
+          "Accompagner une clientèle de particuliers dans la gestion de leurs comptes et de leurs projets financiers.",
+      },
+      {
+        title: "Conseil et vente de produits bancaires :",
+        description:
+          "Proposer des solutions adaptées (épargne, crédits, assurances, etc...) en fonction des besoins et du profil des clients.",
+      },
+      {
+        title: "Analyse et gestion des risques :",
+        description:
+          "Etudier les demandes de financement, analyser la solvabilité des clients et veiller au respect des procédures réglementaires.",
+      },
+      {
+        title: "Suivi et fidélisation :",
+        description:
+          "Assurer un suivi personnalisé, répondre aux sollicitations et anticiper les besoins pour renforcer la relation client.",
+      },
+      {
+        title: "Prospection et développement commercial :",
+        description:
+          "Identifier de nouvelles opportunités, attirer de nouveaux clients et participer aux actions commerciales de l’agence.",
+      },
+      {
+        title: "Accueil et accompagment des clients :",
+        description:
+          "Assurer l’accueil physique et téléphonique, identifier rapidement leurs besoins et leur apporter une réponse immédiate ou les orienter vers le bon interlocuteur.",
+      },
+    ],
+  },
+  {
+    date: "FEVRIER 2024 - AVRIL 2024",
+    poste: "Développeur Full Stack",
+    entreprise: "King UP",
+    lieu: "Plescop (56)",
+    logoSrc: logoKingup,
+    logoAlt: "Logo de King UP SAS",
+    missions: [
+      {
+        title: "Refonte et mise à jour d’un site web :",
+        description:
+          "Développement de la version 4 d’un site (kingmods.com) en utilisant Visual Studio Code, Laravel, Tailwind CSS, JavaScript, Alpine.js, Figma et GitHub. Intégration des maquettes Figma pour assurer une cohérence avec la nouvelle interface.",
+      },
+      {
+        title: "Automatisation de récupération de données :",
+        description:
+          "Création d’un script Python permettant d’extraire automatiquement des informations (textes et images) sur les véhicules et matériels du jeu Farming Simulator. Formatage et structuration des données en JSON pour une exploitation optimisée.",
+      },
+      {
+        title: "Développement d’un site vitrine :",
+        description:
+          "Conception et développement complet à partir d’une maquette figma du site internet de l’entreprise, en appliquant les mêmes technologies que pour le site kingmods.com, dans le but de présenter les projets et compétences de la société.",
+      },
+    ],
+  },
+  {
+    date: "SEPTEMBRE 2022 - SEPTEMBRE 2023",
+    poste: "Apprenti Acheteur",
+    entreprise: "Pythagore",
+    lieu: "Laniscat (22)",
+    logoSrc: logoPythagore,
+    logoAlt: "Logo de Pythagore",
+    missions: [
+      {
+        title: "Création et structuration du service achats :",
+        description:
+          "Mise en place d’un service achats inexistant, définition des processus et des bonnes pratiques pour optimiser les approvisionnements.",
+      },
+      {
+        title: "Développement d’indicateurs et d’outils de suivi :",
+        description:
+          "Élaboration d’indicateurs de performance et d’outils permettant un meilleur pilotage des achats (tableaux de suivi, reporting, analyses de coûts).",
+      },
+      {
+        title:
+          "Gestion des achats de consommables et d’outils pour l’atelier :",
+        description:
+          "Identification des besoins, sélection des fournisseurs et gestion des commandes pour garantir un approvisionnement efficace.",
+      },
+      {
+        title: "Relation et négociation avec les fournisseurs :",
+        description:
+          "Rencontres avec les fournisseurs pour présenter la nouvelle stratégie d’achats, négocier des conditions avantageuses et assurer leur engagement dans la transition du service achats.",
+      },
+    ],
+  },
+  {
+    date: "SEPTEMBRE 2021 - SEPTEMBRE 2022",
+    poste: "Apprenti Acheteur",
+    entreprise: "Les Ateliers du Goût",
+    lieu: "Noyal-Pontivy (56)",
+    logoSrc: logoLAG,
+    logoAlt: "Logo des Ateliers du Goût",
+    missions: [
+      {
+        title: "Participation à la gestion des achats indirects :",
+        description:
+          "Contribution aux activités du service achats, suivi des dossiers et support aux équipes en place.",
+      },
+      {
+        title: "Mise en place d’indicateurs de suivi :",
+        description:
+          "Développement d’outils pour surveiller l’évolution des prix du gaz et de l’électricité et aider à la prise de décision.",
+      },
+      {
+        title: "Animation et participation aux réunions :",
+        description:
+          "Organisation et animation de certaines réunions, élaboration de comptes rendus pour assurer le suivi des décisions prises.",
+      },
+      {
+        title: "Sourcing et évaluation des fournisseurs :",
+        description:
+          "Identification de nouveaux fournisseurs, élaboration de grilles de notation pour comparer et sélectionner les partenaires les plus adaptés.",
+      },
+      {
+        title: "Négociation et contractualisation :",
+        description:
+          "Rencontres avec les fournisseurs pour négocier les conditions d’achat avant la mise en place des contrats.",
+      },
+      {
+        title: "Gestion des contrats :",
+        description:
+          "Suivi des échéances, analyse des durées, gestion des renouvellements tacites ou non.",
+      },
+    ],
+  },
+  {
+    date: "SEPTEMBRE 2020 - SEPTEMBRE 2021",
+    poste: "Apprenti en Logistique",
+    entreprise: "Pythagore",
+    lieu: "Laniscat (22)",
+    logoSrc: logoPythagore,
+    logoAlt: "Logo de Pythagore",
+    missions: [
+      {
+        title: "Mise en place d’une gestion des stocks :",
+        description:
+          "Développement d’outils et de méthodes pour optimiser le suivi des stocks et améliorer leur gestion.",
+      },
+      {
+        title: "Collaboration avec un développeur :",
+        description:
+          "Participation à la création d’un système de calepinage permettant de calculer la consommation de matière première par commande client.",
+      },
+      {
+        title: "Optimisation du suivi des stocks :",
+        description:
+          "Conception et mise en place d’étiquettes améliorées pour assurer une traçabilité plus efficace des produits.",
+      },
+    ],
+  },
+];
---
+++ src/data/formations.ts
@@ -0,0 +1,38 @@
+export const formations = [
+  {
+    date: "SEPTEMBRE 2025 - SEPTEMBRE 2027",
+    formation: "BTS SIO, Option SLAM",
+    ecole: "ESNA",
+    lieu: "Bruz (35)",
+  },
+  {
+    date: "SEPTEMBRE 2023 - AOUT 2024",
+    formation: "Bachelor Développeur Full Stack",
+    ecole: "Studi",
+    lieu: "Distanciel",
+  },
+  {
+    date: "SEPTEMBRE 2021 - SEPTEMBRE 2023",
+    formation: "Master Achats",
+    ecole: "ESLI",
+    lieu: "Redon (35)",
+  },
+  {
+    date: "SEPTEMBRE 2020 - SEPTEMBRE 2021",
+    formation: "Licence logistique",
+    ecole: "ESLI",
+    lieu: "Redon (35)",
+  },
+  {
+    date: "SEPTEMBRE 2017 - MARS 2020",
+    formation: "Licence droit, économie & gestion",
+    ecole: "Université Bretagne Sud",
+    lieu: "Vannes (56)",
+  },
+  {
+    date: "SEPTEMBRE 2014 - SEPTEMBRE 2017",
+    formation: "BAC Economique & Social",
+    ecole: "Lycée Joseph Loth",
+    lieu: "Pontivy (56)",
+  },
+];
